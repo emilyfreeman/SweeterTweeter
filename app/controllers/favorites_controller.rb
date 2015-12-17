e37fd8393791a16408660b3d46958d5de1d42bc3
Emily Dowdle <emily@editingemily.com>
2015-12-17 05:20:09 -0700

    Use current_user

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,11 @@
 class FavoritesController < ApplicationController
   def create
-    twitter_service.favorite(params[:tweet_id])
+    current_user.favorite(params[:tweet_id])
     redirect_to profile_path
   end
 
   def destroy
-    twitter_service.unfavorite(params[:tweet_id])
+    current_user.unfavorite(params[:tweet_id])
     redirect_to profile_path
   end
-
-  private
-
-    def twitter_service
-      TwitterService.new(current_user).client
-    end
 end
